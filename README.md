f74f8a51498d90ab4eba7be8cfb07f02948cfab2
ganyaotong <490852179@qq.com>
2017-08-03 08:22:08 +0800

    add page.js

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # iidopage
 iidopage is library for generate page menu
+
+##demo
+``` html
+<ul id="menu" class="pageNavitor">
+</ul>
+```
+
+``` javascript
+new pageMenu('menu').generate(5,1);
+```
+
+output:
+``` html
+<ul id="menu" class="pageNavitor">
+	<li class="current">1</li>
+	<li>2</li>
+	<li>3</li>
+	<li>4</li>
+	<li>5</li>
+</ul>
+```
---
+++ page.js
@@ -0,0 +1,51 @@
+/*
+iidoMenu is a generator for page menu
+you need to customize callback
+*/
+var iidoMenu = function iidoMenu(pageMenuId){
+	this.pageMenuId = pageMenuId;
+	return this;
+}
+
+iidoMenu.prototype.generate = function(pageCount,current) {
+	current = parseInt(current);
+	var html = "";
+	var startHtml = "";
+	var currentHtml = "<li class='current'>"+current+"</li>";
+	var stopHtml = "";
+	if (current==1) {
+		startHtml = "";
+	}else{
+		var count = 4;
+		 for (var i = current-1; i >= 1; i--) {
+		 	if (count==0) {
+		 		break;
+		 	}
+			startHtml = "<li>"+i+"</li>"+startHtml;
+			count --;
+		} 
+	}
+
+	for (var i = current+1; i < current+5; i++) {
+		if (i>pageCount) {
+			break;
+		}
+		stopHtml += "<li>"+i+"</li>";
+	}
+
+	html = startHtml+currentHtml+stopHtml;
+	
+	document.getElementById(this.pageMenuId).innerHTML = html;
+
+	//you need to customize here
+	$('#menu li').click(function(even){
+		var c = $(this).text();
+		getPage(c);
+		getMenu(c);
+	});
+};
+
+
+function pageMenu(pageMenuId){
+	return new iidoMenu(pageMenuId);
+}
